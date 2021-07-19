fc14fb02fd43eb20552413d96c55b3cbcabd74b7
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-18 18:18:32 -0700

    add cursor details

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 const filterCursor = document.querySelector(".cursor");
 const mainImageDiv = document.querySelector(".main__img");
 
-mainImageDiv.addEventListener('mouseover', function(e) {
+console.log(filterCursor);
+console.log(mainImageDiv);
+
+filterCursor.addEventListener('mouseover', function(e) {
     e.preventDefault;
     filterCursor.style.top = e.pageY + "px";
     filterCursor.style.left = e.pageX +"px";
-    //mainImageDiv.style.opacity = 1;
+    filterCursor.style.backgroundColor = "";
 })
