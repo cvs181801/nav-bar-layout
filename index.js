2fdeabcf6fa10f5eeeb03dd6176dcbd01d14ebfa
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-19 16:27:15 -0700

    add filter property in js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log(mainImageDiv);
 
 filterCursor.addEventListener('mouseover', function(e) {
     e.preventDefault;
-    filterCursor.style.top = e.pageY + "px";
-    filterCursor.style.left = e.pageX +"px";
-    filterCursor.style.backgroundColor = "";
+    filterCursor.style.top = e.pageY + 1 + "px";
+    filterCursor.style.left = e.pageX + "px";
+    filterCursor.style.filter = "brightness(0.4)";
 })
