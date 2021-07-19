feac59d0ae8d84dff2783d7fc6d462297c7feeb4
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-19 16:33:20 -0700

    try removing filter from js

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ filterCursor.addEventListener('mouseover', function(e) {
     e.preventDefault;
     filterCursor.style.top = e.pageY + 1 + "px";
     filterCursor.style.left = e.pageX + "px";
-    filterCursor.style.filter = "brightness(0.4)";
+    //filterCursor.style.filter = "brightness(0.4)";
 })
