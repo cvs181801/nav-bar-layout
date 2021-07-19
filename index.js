3fcda7f324d0ff87a204919e779eb3f84fdfd0ac
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-18 18:10:36 -0700

    add a cursor

---
+++ index.js
@@ -0,0 +1,11 @@
+//grab elements off the DOM
+
+const filterCursor = document.querySelector(".cursor");
+const mainImageDiv = document.querySelector(".main__img");
+
+mainImageDiv.addEventListener('mouseover', function(e) {
+    e.preventDefault;
+    filterCursor.style.top = e.pageY + "px";
+    filterCursor.style.left = e.pageX +"px";
+    mainImageDiv.style.opacity = 0;
+})
