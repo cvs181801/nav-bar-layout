b47ea06ade2bd4d0f1fcb8a5f8d07d6c367e5a2f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-19 18:49:56 -0700

    add different kind of js function

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 //grab elements off the DOM
 
-const filterCursor = document.querySelector(".cursor");
+let filterCursor = document.querySelector(".cursor");
 const mainImageDiv = document.querySelector(".main__img");
 
 console.log(filterCursor);
 console.log(mainImageDiv);
 
-mainImageDiv.addEventListener('mouseover', function(e) {
-    e.preventDefault;
-    filterCursor.style.top = e.pageY + 1 + "px";
-    filterCursor.style.left = e.pageX + "px";
-    //filterCursor.style.filter = "brightness(0.4)";
-})
+// mainImageDiv.addEventListener('mouseover', function(e) {
+    
+//     e.preventDefault;
+//     filterCursor.style.top = e.pageY + 1 + "px";
+//     filterCursor.style.left = e.pageX + "px";
+   
+// })
+
+document.getElementById("main__img").onmousemove = cursor.mouseMove;
+
+cursor.mouseMove = function(mouseMovement) {
+    filterCursor = document.querySelector(".cursor");
+    let xPosition;
+    let yPosition;
+    if(mouseMovement) {
+        xPosition = mouseMovement.pageX;
+        yPosition = mouseMovement.pageY;
+        filterCursor.style.top = yPosition + 1 + "px";
+        filterCursor.style.left = xPosition + "px";
+    }
+}
