d5a42ac3541b702265002f8737605b771812807f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-19 19:36:39 -0700

    add animation

--- index.js
+++ index.js
@@ -2,38 +2,25 @@
 
 let filterCursor = document.querySelector(".cursor");
 const mainImageDiv = document.querySelector(".main__img");
+const valueBuilder = document.querySelectorAll(".value__build");
 
 console.log(filterCursor);
 console.log(mainImageDiv);
 
-// mainImageDiv.addEventListener('mouseover', function(e) {
-    
-//     e.preventDefault;
-//     filterCursor.style.top = e.pageY + 1 + "px";
-//     filterCursor.style.left = e.pageX + "px";
-   
-// })
-
-//try again
-// document.getElementById("main__img").onmousemove = filterCursor.mouseMove;
-
-// filterCursor.mouseMove = function(mouseMovement) {
-//     filterCursor = document.querySelector(".cursor");
-//     let xPosition;
-//     let yPosition;
-//     if(mouseMovement) {
-//         xPosition = mouseMovement.pageX;
-//         yPosition = mouseMovement.pageY;
-//         filterCursor.style.top = yPosition + 1 + "px";
-//         filterCursor.style.left = xPosition + "px";
-//     }
-// }
-
-//try again
-
 window.addEventListener("mousemove", cursor);
 
+//make the cursor work
 function cursor(e) {
     filterCursor.style.top = e.pageY + 'px';
     filterCursor.style.left = e.pageX + 'px';
 }
+
+//make the cursor grow and the colors change when hovering over the value builders
+valueBuilder.forEach(div => {
+    div.addEventListener('mouseleave', () => {
+        classList.remove("link__grow");
+    });
+    div.addEventListener('mouseover', () => {
+        classList.add("link__grow");
+    });
+});
