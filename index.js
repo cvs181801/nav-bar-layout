bc3f09ac2e42863ed968de2500fbe7a1eb41c322
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-21 20:25:29 -0700

    add JS

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 let filterCursor = document.querySelector(".cursor");
 const mainImageDiv = document.querySelector(".main__img");
 const valueBuilder = document.querySelectorAll(".value__build");
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".nav__bar");
+const hamBar1 = document.querySelector(".first__bar");
+const hamBar2 = document.querySelector(".second__bar");
+const hamBar3 = document.querySelector(".third__bar");
+const mainDiv = document.querySelector(".main__div");
 
 console.log(valueBuilder);
 
@@ -15,7 +21,7 @@ function cursor(e) {
     filterCursor.style.left = e.pageX + 'px';
 }
 
-//make the cursor grow and the colors change when hovering over the value builders
+//make the cursor grow and the colors change when hovering over the red "value builders"
 valueBuilder.forEach(div => {
     div.addEventListener('mouseleave', () => {
         filterCursor.classList.remove("link__grow");
@@ -24,3 +30,15 @@ valueBuilder.forEach(div => {
         filterCursor.classList.add("link__grow");
     });
 });
+
+//make the nav bar open and close
+
+hamburger.addEventListener('click', function(e) {
+    e.preventDefault;
+    navBar.classList.add("open");
+    hamBar1.classList.add("change");
+    hamBar2.classList.add("change");
+    hamBar3.classList.add("change");
+    mainDiv.classList.add("shift");
+})
+
