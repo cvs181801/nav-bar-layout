659b2e839f4cf6ded6fd7643dacc0842881b2cc6
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-27 14:23:08 -0700

    try changing back to click

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ for(let i = 0; i < navBarLength; i++ ) {
 
 //make the modal buttons work
 
-submitBtn.addEventListener('submit', function() {
+submitBtn.addEventListener('click', function() {
     console.log("submit btn worked!");
     //e.preventDefault();
     // modalElements.forEach( element => {
